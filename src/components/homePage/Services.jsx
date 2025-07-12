fe30fa746a1a62292663042355311f8a9f14f7ce
firoz.nur <firoznur5@gmail.com>
2025-07-12 22:42:40 +0600

    Use : Database

--- src/components/homePage/Services.jsx
+++ src/components/homePage/Services.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { services } from "../../lib/services";
+// import { services } from "../../lib/services";
 import ServiceCard from "./ServiceCard";
+
+const getServices = async () => {
+  const res = await fetch("http://localhost:3000/services/api/getAll");
+  const services = await res.json();
+  return services;
+};
+
 const Services = async () => {
+  const { services } = await getServices();
+
   if (services?.length <= 0) {
     return null;
   }
 
   return (
-    <div className="mb-24 text-slate-800 mt-32">
+    <div className="mt-32 mb-24 text-slate-800">
       <div className="container mx-auto text-center">
-        <h3 className="text-xl font-bold text-primary">Our Services</h3>
-        <h2 className="text-5xl font-bold text-Dark-01 mt-5">Our Service Area</h2>
-        <p className="mt-5 leading-7 text-Dark-03 capitalize">
+        <h3 className="text-primary text-xl font-bold">Our Services</h3>
+        <h2 className="text-Dark-01 mt-5 text-5xl font-bold">
+          Our Service Area
+        </h2>
+        <p className="text-Dark-03 mt-5 leading-7 capitalize">
           the majority have suffered alteration in some form, by injected
           humour, or randomised <br /> words which do not look even slightly
           believable.{" "}
