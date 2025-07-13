4a8f7bd986f8605e1a785cd9e9badd31716ac7f3
firoz.nur <firoznur5@gmail.com>
2025-07-13 07:27:15 +0600

    Get : Single Service

---
+++ src/app/services/api/[id]/route.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/lib/ConnectDB";
+import { NextResponse } from "next/server";
+
+export const GET = async (request,{params}) => {
+  const db = await connectDB();
+  const servicesCollection = db.collection("services");
+  try {
+    const services = await servicesCollection.findOne({_id: params.id})
+    return NextResponse.json({ services });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "No Data Found", error });
+  }
+};
