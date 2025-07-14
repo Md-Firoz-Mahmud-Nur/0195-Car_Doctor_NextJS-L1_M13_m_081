21e6054c251cf278254b180683eb0de9bfdaf4ad
firoz.nur <firoznur5@gmail.com>
2025-07-14 11:07:03 +0600

    Add : Form

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -4,8 +4,34 @@ import Hero from "@/components/shared/Hero";
 const page = () => {
   return (
     <div className="container mx-auto my-12">
-      <div>
-        <Hero title={"Check Out"} path={"Home/Checkout"}></Hero>;
+      <Hero title={"Check Out"} path={"Home/Checkout"}></Hero>
+      <div className="bg-Dark-07 mt-32 rounded-lg p-24">
+        <div className="grid grid-cols-2 gap-6">
+          <input
+            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+            placeholder="First Name"
+          ></input>
+          <input
+            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+            placeholder="Last Name"
+          ></input>
+          <input
+            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+            placeholder="Your Phone"
+          ></input>
+          <input
+            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+            placeholder="Your Email"
+          ></input>
+          <textarea
+            rows={5}
+            placeholder="Your Message"
+            className="placeholder:text-Dark-04 col-span-2 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+          ></textarea>
+          <button className="bg-primary col-span-2 rounded-lg px-6 py-4 text-xl leading-7 font-semibold text-white">
+            Order Confirm
+          </button>
+        </div>
       </div>
     </div>
   );
