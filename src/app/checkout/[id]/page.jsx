3dfd70b9101ad4622660c51fb952e754802351fa
firoz.nur <firoznur5@gmail.com>
2025-07-14 16:24:29 +0600

    Console : Form Data

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -16,6 +16,22 @@ const Checkout = ({ params }) => {
   };
   const { _id, title, description, img, price, facility } = service || {};
 
+  const handleBooking = async (event) => {
+    event.preventDefault();
+    const newBooking = {
+      email: data?.user?.email,
+      name: data?.user?.name,
+      service: title,
+      serviceId: _id,
+      price: price,
+      phone: event.target.phone.value,
+      address: event.target.address.value,
+      dueDate: event.target.dueDate.value,
+      message: event.target.message.value,
+    };
+    console.log(newBooking);
+  };
+
   useEffect(() => {
     loadService();
   }, [id]);
@@ -24,35 +40,56 @@ const Checkout = ({ params }) => {
     <div className="container mx-auto my-12">
       <Hero title={"Check Out"} path={"Home/Checkout"}></Hero>
       <div className="bg-Dark-07 mt-32 rounded-lg p-24">
-        <div className="grid grid-cols-2 gap-6">
-          <input
-            defaultValue={data?.user?.name}
-            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
-            placeholder="First Name"
-          ></input>
-          <input
-            defaultValue={price}
-            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
-            placeholder="Price"
-          ></input>
-          <input
-            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
-            placeholder="Your Phone"
-          ></input>
-          <input
-            defaultValue={data?.user?.email}
-            className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
-            placeholder="Your Email"
-          ></input>
-          <textarea
-            rows={5}
-            placeholder="Your Message"
-            className="placeholder:text-Dark-04 col-span-2 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
-          ></textarea>
-          <button className="bg-primary col-span-2 rounded-lg px-6 py-4 text-xl leading-7 font-semibold text-white">
-            Order Confirm
-          </button>
-        </div>
+        <form onSubmit={handleBooking}>
+          <div className="grid grid-cols-2 gap-6">
+            <input
+              defaultValue={data?.user?.name}
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Name"
+            ></input>
+            <input
+              defaultValue={price}
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Price"
+              readOnly
+            ></input>
+            <input
+              name="phone"
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Your Phone"
+              required
+            ></input>
+            <input
+              defaultValue={data?.user?.email}
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Your Email"
+              readOnly
+            ></input>
+            <input
+              name="address"
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Address"
+              required
+            ></input>
+            <input
+              defaultValue={new Date().toISOString().split("T")[0]}
+              type="date"
+              name="dueDate"
+              className="placeholder:text-Dark-04 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+              placeholder="Due Date"
+              required
+            ></input>
+            <textarea
+              name="message"
+              rows={5}
+              placeholder="Your Message"
+              className="placeholder:text-Dark-04 col-span-2 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
+            ></textarea>
+            <button className="bg-primary col-span-2 rounded-lg px-6 py-4 text-xl leading-7 font-semibold text-white">
+              Order Confirm
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
