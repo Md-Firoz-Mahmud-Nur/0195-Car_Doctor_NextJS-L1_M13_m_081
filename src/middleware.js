ff30a634d7074b233184ae21cdf77ecca3fce774
firoz.nur <firoznur5@gmail.com>
2025-07-15 20:21:32 +0600

    Add : Axios

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,30 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
-export const middleware = async (request) => {
-  const token = cookies(request).get("__Secure-next-auth.session-token");
+export function middleware(request) {
+  const token =
+    cookies().get("__Secure-next-auth.session-token") ||
+    cookies().get("next-auth.session-token");
+
   const pathname = request.nextUrl.pathname;
-  if (pathname.includes("api")) {
+
+  // Allow all API routes to pass through
+  if (pathname.startsWith("/api")) {
     return NextResponse.next();
   }
 
+  // If not authenticated, redirect to login
   if (!token) {
     return NextResponse.redirect(
       new URL(`/login?redirect=${pathname}`, request.url),
     );
   }
-  return NextResponse.next();
-};
 
+  return NextResponse.next();
+}
+
+// Only apply middleware to protected routes
 export const config = {
   matcher: ["/my-booking/:path*", "/services/:path*"],
 };
--- src/services/getServices.js
+++ src/services/getServices.js
@@ -1,11 +1,29 @@
+import axios from "axios";
+
 export const getServices = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/services/api/getAll`);
+  try {
+    const res = await axios.get(`${process.env.BASE_URL}/services/api/getAll`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const getServiceDetails = async (id) => {
+  const res = await fetch();
+  if (!res.ok) {
+    throw new Error(`Failed to fetch: ${res.status}`);
+  }
   const services = await res.json();
   return services;
 };
 
-export const getServiceDetails = async (id) => {
-  const res = await fetch(`${process.env.BASE_URL}/services/api/${id}`);
-  const services = await res.json();
-  return services;
+export const getServicesDetails = async (id) => {
+  try {
+    const res = await axios.get(`${process.env.BASE_URL}/services/api/${id}`);
+    return res.data;
+  } catch (error) {
+    return {};
+  }
 };
