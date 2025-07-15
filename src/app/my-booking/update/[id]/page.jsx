89a3ebc1d531c90ab8008b7bfb7bdf7a4900a77b
firoz.nur <firoznur5@gmail.com>
2025-07-15 16:03:46 +0600

    Update : Booking

--- src/app/my-booking/update/[id]/page.jsx
+++ src/app/my-booking/update/[id]/page.jsx
@@ -1,18 +1,41 @@
 "use client";
 import Hero from "@/components/shared/Hero";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const page = ({ params }) => {
+  const { id } = use(params);
   const { data } = useSession();
   const [booking, setBooking] = useState({});
   const loadBooking = async () => {
-    const { id } = await params;
     const bookingDetails = await fetch(
       `http://localhost:3000/my-booking/api/booking/${id}`,
     );
     const data = await bookingDetails.json();
     setBooking(data?.data);
+  };
+
+  const handleUpdateBooking = async (event) => {
+    event.preventDefault();
+    const updatedBooking = {
+      dueDate: event.target.dueDate.value,
+      phone: event.target.phone.value,
+      address: event.target.address.value,
+    };
+    const resp = await fetch(
+      `http://localhost:3000/my-booking/api/booking/${id}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(updatedBooking),
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+    );
+    if (resp.status === 200) {
+      toast.success("Updated Successfully");
+    }
   };
 
   useEffect(() => {
@@ -23,7 +46,7 @@ const page = ({ params }) => {
     <div className="container mx-auto my-12">
       <Hero title={"Edit Booking"} path={"Home/Edit Booking"}></Hero>
       <div className="bg-Dark-07 mt-32 rounded-lg p-24">
-        <form>
+        <form onSubmit={handleUpdateBooking}>
           <div className="grid grid-cols-2 gap-6">
             <input
               defaultValue={data?.user?.name}
@@ -73,11 +96,12 @@ const page = ({ params }) => {
               className="placeholder:text-Dark-04 col-span-2 rounded-lg bg-white px-6 py-4 placeholder:leading-7"
             ></textarea>
             <button className="bg-primary col-span-2 rounded-lg px-6 py-4 text-xl leading-7 font-semibold text-white">
-              Order Confirm
+              Update
             </button>
           </div>
         </form>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
