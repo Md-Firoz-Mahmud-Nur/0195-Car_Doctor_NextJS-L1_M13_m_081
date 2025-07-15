075e6d834cb6a01ba76dfda8561c32ef3a000252
firoz.nur <firoznur5@gmail.com>
2025-07-15 18:54:16 +0600

    Add : Under Construction Page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p className="text-shadow-Dark-01 flex h-[50vh] w-[50vw] items-center justify-center rounded-lg bg-[radial-gradient(circle,_#FF3811,_white_50%)] text-center text-2xl text-shadow-2xs">
+        This is contact page and
+        <br /> under development
+      </p>
+    </div>
+  );
+};
+
+export default page;
